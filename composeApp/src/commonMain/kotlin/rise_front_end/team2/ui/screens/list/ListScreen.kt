5d14c0409d94b3d0c1cb3297f1488a56bd0cb994
Yessboom <Tanguy.guissard@hotmail.be>
2024-11-14 14:51:15 +0100

    Modified Material to Material3

--- composeApp/src/commonMain/kotlin/rise_front_end/team2/ui/screens/list/ListScreen.kt
+++ composeApp/src/commonMain/kotlin/rise_front_end/team2/ui/screens/list/ListScreen.kt
@@ -3,22 +3,11 @@ package rise_front_end.team2.ui.screens.list
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.WindowInsetsSides
-import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.only
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -33,22 +22,25 @@ import io.kamel.image.asyncPainterResource
 import org.koin.compose.viewmodel.koinViewModel
 import rise_front_end.team2.data.SyllabusObject
 import rise_front_end.team2.ui.screens.EmptyScreenContent
+import rise_front_end.team2.ui.theme.AppTheme
 
 @Composable
 fun ListScreen(
     navigateToDetails: (objectId: Int) -> Unit
 ) {
-    val viewModel = koinViewModel<ListViewModel>()
-    val objects by viewModel.objects.collectAsState()
+    AppTheme {
+        val viewModel = koinViewModel<ListViewModel>()
+        val objects by viewModel.objects.collectAsState()
 
-    AnimatedContent(objects.isNotEmpty()) { objectsAvailable ->
-        if (objectsAvailable) {
-            ObjectGrid(
-                objects = objects,
-                onObjectClick = navigateToDetails,
-            )
-        } else {
-            EmptyScreenContent(Modifier.fillMaxSize())
+        AnimatedContent(objects.isNotEmpty()) { objectsAvailable ->
+            if (objectsAvailable) {
+                ObjectGrid(
+                    objects = objects,
+                    onObjectClick = navigateToDetails,
+                )
+            } else {
+                EmptyScreenContent(Modifier.fillMaxSize())
+            }
         }
     }
 }
@@ -61,12 +53,10 @@ private fun ObjectGrid(
 ) {
     LazyVerticalGrid(
         columns = GridCells.Adaptive(180.dp),
-        // TODO simplify padding after https://issuetracker.google.com/issues/365052672 is fixed
         modifier = modifier
             .fillMaxSize()
             .padding(WindowInsets.safeDrawing.only(WindowInsetsSides.Horizontal).asPaddingValues()),
-        contentPadding = WindowInsets.safeDrawing.only(WindowInsetsSides.Vertical)
-            .asPaddingValues(),
+        contentPadding = WindowInsets.safeDrawing.only(WindowInsetsSides.Vertical).asPaddingValues(),
     ) {
         items(objects, key = { it.objectID }) { obj ->
             ObjectFrame(
@@ -101,10 +91,16 @@ private fun ObjectFrame(
         Spacer(Modifier.height(2.dp))
 
         Text(
-            obj.title,
-            style = MaterialTheme.typography.subtitle1.copy(fontWeight = FontWeight.Bold)
+            text = obj.title,
+            style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold)
         )
-        Text(obj.artistDisplayName, style = MaterialTheme.typography.body2)
-        Text(obj.objectDate, style = MaterialTheme.typography.caption)
+        Text(
+            text = obj.artistDisplayName,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Text(
+            text = obj.objectDate,
+            style = MaterialTheme.typography.bodySmall
+        )
     }
 }
